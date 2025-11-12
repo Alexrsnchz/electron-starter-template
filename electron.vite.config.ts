483f63ed5200d06ed24683f1a2e68dcebc8b4b4f
Alexrsnchz <alexrsnchz@gmail.com>
2025-11-12 23:34:20 +0100

    chore: Change the default build folder to dist/

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,15 +3,18 @@ import { defineConfig } from 'electron-vite';
 export default defineConfig({
   main: {
     build: {
+      outDir: 'dist/main',
       lib: { entry: 'src/main/index.ts' },
     },
   },
   preload: {
     build: {
+      outDir: 'dist/preload',
       lib: { entry: 'src/preload/index.ts' },
     },
   },
   renderer: {
+    build: { outDir: 'dist/renderer' },
     plugins: [],
   },
 });
